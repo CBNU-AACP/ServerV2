83b69f5851c17ec4ec3ae9e57436ac5cb15688c9
DongWooE <ehddn977@naver.com>
2022-02-06 18:28:03 +0900

    Feat.(#12)-user service 추가

---
+++ server/src/main/java/aacp/server/user/service/UserService.java
@@ -0,0 +1,41 @@
+package aacp.server.user.service;
+
+import aacp.server.domain.User;
+import aacp.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long register(User user){
+        if(!validateDuplicateUser(user.getIdentifier())) throw new IllegalStateException("이미 존재하는 회원입니다");
+        else{
+            userRepository.save(user);
+            return user.getId();
+        }
+    }
+
+    public boolean validateDuplicateUser(String userInfo){
+        return (userInfo.contains("@")) ?
+                userRepository.findByEmail(userInfo).isEmpty()
+                : userRepository.findByIdentifier(userInfo).isEmpty();
+    }
+
+    public String login(String identifier, String password){
+        List<User> userList = userRepository.findByIdentifier(identifier);
+        if(!userList.isEmpty()){
+            if(!userList.get(0).getPassword().equals(password)) throw new IllegalStateException("비밀번호가 일치하지 않습니다");
+            else return identifier;
+        }
+        else throw new IllegalStateException("존재하지 않는 회원입니다");
+    }
+
+}
