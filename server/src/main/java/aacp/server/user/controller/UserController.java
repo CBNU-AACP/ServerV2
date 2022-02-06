ce2e659a1db49431b61e60eadd13361cbdecaa3c
DongWooE <ehddn977@naver.com>
2022-02-06 18:28:37 +0900

    Feat.(#12)-user controller 추가

---
+++ server/src/main/java/aacp/server/user/controller/UserController.java
@@ -0,0 +1,68 @@
+package aacp.server.user.controller;
+
+import aacp.server.domain.User;
+import aacp.server.user.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid UserRegisterRequestDto request){
+        User user = new User(request.identifier, request.password, request.email, request.studentId, request.schoolCode, request.phoneNumber);
+        userService.register(user);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserResponseDto> login(@RequestBody @Valid UserLoginRequestDto request){
+        try{
+            String userIdentifier = userService.login(request.identifier, request.password);
+            return new ResponseEntity<>(new UserResponseDto(userIdentifier), HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/check/{value}")
+    public ResponseEntity<?> validateDuplicateUser(@PathVariable("value") String identifier){
+        if(userService.validateDuplicateUser(identifier))   return new ResponseEntity<>(HttpStatus.OK);
+        else return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UserRegisterRequestDto{
+        private String identifier;
+        private String password;
+        private String email;
+        private String studentId;
+        private String schoolCode;
+        private String phoneNumber;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UserLoginRequestDto{
+        private String identifier;
+        private String password;
+    }
+
+    @AllArgsConstructor
+    static class UserResponseDto{
+        private String identifier;
+    }
+
+
+}
