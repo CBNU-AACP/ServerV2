b43d61785f811841cc68265cb70c5ae4c3dc6873
DongWooE <ehddn977@naver.com>
2022-02-08 20:07:13 +0900

    Conf. set jwt setting in spring security

--- server/src/main/java/aacp/server/common/config/SecurityConfig.java
+++ server/src/main/java/aacp/server/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -18,11 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().disable()
-                .csrf().disable()
-                .formLogin().disable();
-                //loginProcessingUrl("/api/users/login");
-                //.headers().frameOptions().disable()
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable();
 
         http.authorizeRequests()
                 .antMatchers("/api/users/**").permitAll()
