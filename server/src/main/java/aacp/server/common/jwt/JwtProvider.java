051cf30ff56be83f33474ad6a8ff5d71de06db56
DongWooE <ehddn977@naver.com>
2022-02-12 23:27:54 +0900

    Feat.(#15)- jwtProvider의 몇 가지 메소드에 기능 추가

--- server/src/main/java/aacp/server/common/jwt/JwtProvider.java
+++ server/src/main/java/aacp/server/common/jwt/JwtProvider.java
@@ -1,18 +1,31 @@
 package aacp.server.common.jwt;
 
+import aacp.server.common.jwt.impl.UserDetailsSerivceImpl;
+import aacp.server.domain.User;
+import aacp.server.user.repository.UserRepository;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class JwtProvider {
 
     private final String jwtSecretKey = "accp-secret";
     private final Long jwtExpirationTime = 1200000L;
+    private final UserDetailsService userDetailsService;
 
     public String getJwtToken(String identifier){
         return JWT.create()
@@ -25,5 +38,24 @@ public class JwtProvider {
     public String verifyJwtToken(String token){
         return JWT.require(Algorithm.HMAC256(jwtSecretKey)).build()
                 .verify(token).getClaim("id").asString();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        return request.getHeader("x-access-token");
+    }
+
+    public boolean validateToken(String jwtToken){
+        try{
+            Date expiresAt = JWT.require(Algorithm.HMAC256(jwtSecretKey)).build()
+                    .verify(jwtToken).getExpiresAt();
+            return !expiresAt.before(new Date());
+        }catch(Exception e){
+            return false;
+        }
+    }
+
+    public Authentication getAuthenticiation(String userIdentifier) {
+        UserDetails userDetails = userDetailsService.loadUserByUsername(userIdentifier);
+        return new UsernamePasswordAuthenticationToken(userIdentifier, "", userDetails.getAuthorities());
     }
 }
