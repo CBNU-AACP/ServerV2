0258bd84635e6baf3ed761ed5d5ef90c17c1dc9f
DongWooE <ehddn977@naver.com>
2022-02-15 19:41:51 +0900

    Fix.(#15)-filter 코드 수정

--- server/src/main/java/aacp/server/common/jwt/JwtAuthorizationFilter.java
+++ server/src/main/java/aacp/server/common/jwt/JwtAuthorizationFilter.java
@@ -1,30 +1,25 @@
 package aacp.server.common.jwt;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.filter.GenericFilterBean;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class JwtAuthorizationFilter extends BasicAuthenticationFilter {
-
-    // BasicAuthenticationFilter -> 시큐리티 filter 중에 하나로 권한이나 인증이 필요한 특정 주소를 요청했을때 이 필터를 탄다
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends GenericFilterBean {
 
     private final JwtProvider jwtProvider;
 
-    public JwtAuthorizationFilter(AuthenticationManager authenticationManager, JwtProvider jwtProvider) {
-        super(authenticationManager);
-        this.jwtProvider = jwtProvider;
-    }
-
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-        super.doFilterInternal(request, response, chain);
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         String token = jwtProvider.resolveToken((HttpServletRequest) request);
         if(token != null && jwtProvider.validateToken(token)){
             String userIdentifier = jwtProvider.verifyJwtToken(token);
